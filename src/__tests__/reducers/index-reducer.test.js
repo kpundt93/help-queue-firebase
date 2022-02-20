1d7758442fd63a61ac99abd010b60c54e5ed8d46
Katie Pundt <kzpundt@gmail.com>
2022-02-19 16:11:42 -0800

    refactor project to use Firebase & Firestore

---
+++ src/__tests__/reducers/index-reducer.test.js
@@ -0,0 +1,27 @@
+import rootReducer from '../../reducers/index';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import ticketListReducer from '../../reducers/ticket-list-reducer';
+import { createStore } from 'redux';
+import * as c from './../../actions/ActionTypes';
+
+let store = createStore(rootReducer);
+
+describe("rootReducer", () => {
+
+  test('Check that initial state of ticketListReducer matches root reducer', () => {
+    expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    const action = {
+      type: c.TOGGLE_FORM
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
+
+});
---
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -0,0 +1,36 @@
+import ticketListReducer from '../../reducers/ticket-list-reducer';
+import * as c from './../../actions/ActionTypes';
+
+describe('ticketListReducer', () => {
+
+  let action;
+
+  const currentState = {
+    1: {names: 'Ryan & Aimen',
+    location: '4b',
+    issue: 'Redux action is not working correctly.',
+    id: 1 },
+    2: {names: 'Jasmine and Justine',
+    location: '2a',
+    issue: 'Reducer has side effects.',
+    id: 2 }
+  }
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(ticketListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully delete a ticket', () => {
+    action = {
+      type: c.DELETE_TICKET,
+      id: 1
+    };
+    expect(ticketListReducer(currentState, action)).toEqual({
+      2: {names: 'Jasmine and Justine',
+        location: '2a',
+        issue: 'Reducer has side effects.',
+        id: 2 }
+    });
+  });
+
+});
