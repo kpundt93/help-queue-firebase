17a27a892f5ff0f35cc5d12bc5f1c321d1d3bfe5
Katie Pundt <kzpundt@gmail.com>
2022-02-19 17:56:06 -0800

    add Firebase authentication & authorization

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,8 +1,63 @@
 import React from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 function Signin() {
+  function doSignUp(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const auth = getAuth();
+
+    createUserWithEmailAndPassword(auth, email, password).then(function(){
+      console.log("successfully signed up");
+    }).catch(function(error) {
+      console.log(error.message);
+    });
+  }
+
+  function doSignIn(event) {
+    event.preventDefault();
+    const email = event.target.signInEmail.value;
+    const password = event.target.signInPassword.value;
+    const auth = getAuth();
+
+    signInWithEmailAndPassword(auth, email, password).then(function() {
+      console.log("successfully signed in");
+    }).catch(function(error) {
+      console.log(error.message);
+    });
+  }
+
+  function doSignOut(event) {
+    event.preventDefault();
+    const auth = getAuth();
+
+    signOut(auth).then(function() {
+      console.log("successfully signed out");
+    }).catch(function(error) {
+      console.log(error.message);
+    });
+  }
+
   return (
-    <h1>Sign In</h1>
+    <React.Fragment>
+      <h1>Sign Up</h1>
+      <form onSubmit={doSignUp}>
+        <input type="text" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit">Sign Up</button>
+      </form>
+
+      <h1>Sign In</h1>
+      <form onSubmit={doSignIn}>
+        <input type="text" name="signInEmail" placeholder="Email" />
+        <input type="password" name="signInPassword" placeholder="Password" />
+        <button type="submit">Sign In</button>
+      </form>
+
+      <h1>Sign Out</h1>
+      <button onClick={doSignOut}>Sign Out</button>
+    </React.Fragment>
   );
 }
 
